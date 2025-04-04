7ae0eb6145fb5def5c9fa7c621ae910e941e5124
ErikHrazdira <h.erik@email.cz>
2025-04-04 11:54:20 +0200

    loading from json added

---
+++ STIN-Burza/Controllers/StockController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using STIN_Burza.Models;
+using STIN_Burza.Services;
+
+
+namespace STIN_Burza.Controllers
+{
+    public class StockController : Controller
+    {
+        private StockService stockService = new StockService();
+
+        public IActionResult Index()
+        {
+            var stocks = stockService.LoadFavoriteStocks();
+            if (stocks == null)
+            {
+                stocks = new List<Stock>(); // Pokud je seznam null, nastavíme prázdný seznam
+            }
+            return View(stocks);
+        }
+    }
+}
---
+++ STIN-Burza/Services/StockService.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using STIN_Burza.Models;
+using System.Xml;
+using Formatting = Newtonsoft.Json.Formatting;
+
+namespace STIN_Burza.Services
+{
+    public class StockService
+    {
+        private readonly string filePath = "App_Data/favorite_stocks.json";
+
+        // nacte oblibene ze souboru
+        public List<Stock> LoadFavoriteStocks()
+        {
+            if (File.Exists(filePath))
+            {
+                var json = File.ReadAllText(filePath);
+                return JsonConvert.DeserializeObject<List<Stock>>(json) ?? new List<Stock>(); // Pokud JSON je null, vrátí prázdný seznam
+            }
+            return new List<Stock>(); //kdyz neexistuje tak vrati prazdny
+        }
+
+    }
+}
